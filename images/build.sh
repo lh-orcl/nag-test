ac98f296cce6b12751b08061c50613125786b0c9
lh-orcl <40397212+lh-orcl@users.noreply.github.com>
2018-06-22 10:05:44 +0100

    Add image to client network

--- images/build.sh
+++ images/build.sh
@@ -5,9 +5,11 @@ set -e
 
 DOCKER_RUN_REPO="lh-orcl"
 DOCKER_RUN_IMAGE="docker-nagios_test"
+DOCKER_NAG_NET="client-net"
 
 docker build -t	$DOCKER_RUN_REPO/$DOCKER_RUN_IMAGE:latest .
 
 docker images
 docker run -d --name "$DOCKER_RUN_IMAGE" -p 80:80 -t "$DOCKER_RUN_REPO/$DOCKER_RUN_IMAGE"
+docker network connect $DOCKER_NAG_NET $DOCKER_RUN_IMAGE
 docker ps -a
